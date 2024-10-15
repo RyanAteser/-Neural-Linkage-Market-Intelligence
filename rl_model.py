af93d569f95eef13367a5a5266701020fbb4ae6d
Ryan Ateser <45744958+RyanAteser@users.noreply.github.com>
2024-10-15 13:50:35 -0700

    Create rl_model.py

---
+++ rl_model.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.optimizers import Adam
+
+class RLAgent:
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.model = self.build_model()
+
+    def build_model(self):
+        model = tf.keras.Sequential()
+        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(learning_rate=0.001))
+        return model
+
+    def act(self, state):
+        return np.argmax(self.model.predict(state)[0])
